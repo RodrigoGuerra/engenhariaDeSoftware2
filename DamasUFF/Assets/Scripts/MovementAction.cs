a0819a9d7e6f0d065c2e70bb2be7bddb5491b248
dudu631 <eduardo9ramos@hotmail.com>
2016-11-26 13:03:27 -0200

    Parte do Effectuate Play

---
+++ DamasUFF/Assets/Scripts/MovementAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementAction : MonoBehaviour {
+
+
+	public House start;
+	public House end;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
